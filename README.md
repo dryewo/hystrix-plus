923d2698f1225b7fdf7488e900813b25b9491d22
Dmitrii Balakhonskii <dnomaid@gmail.com>
2018-09-23 01:44:27 +0200

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# hystrix-plus
+[![Build Status](https://travis-ci.org/dryewo/hystrix-plus.svg?branch=master)](https://travis-ci.org/dryewo/hystrix-plus)
+[![codecov](https://codecov.io/gh/dryewo/hystrix-plus/branch/master/graph/badge.svg)](https://codecov.io/gh/dryewo/hystrix-plus)
+[![Clojars Project](https://img.shields.io/clojars/v/me.dryewo/hystrix-plus.svg)](https://clojars.org/me.dryewo/hystrix-plus)
+
+A Clojure library designed to ... well, that part is up to you.
+
+```clj
+[me.dryewo/hystrix-plus "0.0.0"]
+```
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2018 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject me.dryewo/hystrix-plus "0.0.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies []
+  :plugins [[lein-cloverage "1.0.13"]
+            [lein-shell "0.5.0"]
+            [lein-ancient "0.6.15"]
+            [lein-changelog "0.3.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :deploy-repositories [["releases" :clojars]]
+  :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[me\\.dryewo\\\\/hystrix-plus \"[0-9.]*\"\\\\]/[me\\.dryewo\\\\/hystrix-plus \"${:version}\"]/" "README.md"]}
+  :release-tasks [["shell" "git" "diff" "--exit-code"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["changelog" "release"]
+                  ["update-readme-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["vcs" "push"]])
